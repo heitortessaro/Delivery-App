3833823b24473e18a6fddc223f3afaad79fc2c67
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-22 15:29:08 -0300

    feat: create customer/product route and CustomerProducts page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes as Switch, Navigate } from 'react-router-dom';
 import Login from './Page/Login';
 import Register from './Page/Register';
+import CustomerProducts from './Page/CustomerProducts';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path="/" element={ <Navigate to="/login" /> } />
           <Route exact path="/login" element={ <Login /> } />
           <Route exact path="/register" element={ <Register /> } />
+          <Route exact path="/customer/products" element={ <CustomerProducts /> } />
         </Switch>
       </div>
     </BrowserRouter>
