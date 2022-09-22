e06f9729de901c098bfd95bfc481a29c0c3f761e
kerley <kerley.kls@gmail.com>
2022-09-22 11:44:44 -0300

    fix: url requisition

--- front-end/src/services/loginServices.js
+++ front-end/src/services/loginServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  URL: 'http://localhost:3001/login',
+  URL: 'http://localhost:3001',
 });
 
 export const login = async ({ email, password }) => {
