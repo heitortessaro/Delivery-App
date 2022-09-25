d1c6b15f81a475c8b85f4fdf6896b15196fa9c99
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-25 15:46:38 -0300

    fix: add data-testids and propsvalidation to ProductCard

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -1,30 +1,47 @@
 import React from 'react';
 import './styles/productCard.css';
+import PropTypes from 'prop-types';
 
-export default function ProductCard({ name, value, image, quantity }) {
+export default function ProductCard({ name, value, image, quantity, id }) {
   return (
     <div className="product_card">
       <div className="product_price">
-        <span>{`R$ ${value}`}</span>
+        <span data-testid={ `customer_products__element-card-price-${id}` }>
+          {`R$ ${value}`}
+        </span>
       </div>
       <figure className="product_image_container">
-        <img className="product_image" src={ image } alt={ `produto ${name}` } />
+        <img
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+          className="product_image"
+          src={ image }
+          alt={ `produto ${name}` }
+        />
       </figure>
       <div className="product_card_footer">
-        <p className="product_name">{name}</p>
+        <p
+          className="product_name"
+          data-testid={ `customer_products__element-card-title-${id}` }
+        >
+          {name}
+        </p>
         <div>
           <button
             type="button"
             className="product-btn-left"
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
           >
             -
           </button>
           <div className="product_quantity">
-            <span>{quantity}</span>
+            <span data-testid={ `customer_products__input-card-quantity-${id}` }>
+              {quantity}
+            </span>
           </div>
           <button
             type="button"
             className="product-btn-rigth "
+            data-testid={ `customer_products__button-card-add-item--${id}` }
           >
             +
           </button>
@@ -33,3 +50,11 @@ export default function ProductCard({ name, value, image, quantity }) {
     </div>
   );
 }
+
+ProductCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  quantity: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
