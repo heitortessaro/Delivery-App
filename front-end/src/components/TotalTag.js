6b869a0770d91fdbd69eb3b2027f8b0b5540ba07
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-25 17:56:42 -0300

    feat: create component TotalTag and add style to it
    it will show the sum of prices of items inside the cart

---
+++ front-end/src/components/TotalTag.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles/totalTag.css';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+
+export default function TotalTag(
+  { total },
+) {
+  const navigate = useNavigate();
+  return (
+    <button
+      type="button"
+      data-testid="customer_products__checkout-bottom-value"
+      className="total_tag"
+      onClick={ () => navigate() }
+    >
+      {`Ver Carrinho: R$ ${total}`}
+    </button>
+  );
+}
+
+TotalTag.propTypes = {
+  total: PropTypes.number.isRequired,
+};
