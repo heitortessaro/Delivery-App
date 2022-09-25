8161005505963e4abed219e58edad9053dd203a6
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-25 17:40:37 -0300

    feat: Update CustomerProducts page
    start using the ProducCard. Define states for products and total in the cart. Create function to update the products quantity and total in the cart. Use useEffect to catch call the fetch of products.

--- front-end/src/Page/CustomerProducts.js
+++ front-end/src/Page/CustomerProducts.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavClient from '../components/NavClient';
 import ProductCard from '../components/ProductCard';
+import { getProducts, computeTotalCart } from '../services/productsServices';
 import './styles/customerProduct.css';
 
 export default function CustomerProducts() {
+  const [products, setProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const receiveProducts = async () => {
+    const receivedProducts = await getProducts();
+    setProducts(receivedProducts);
+  };
+
+  const changeQuantity = (i, addQtty) => {
+    const newQtty = products[i].quantity + addQtty;
+    if (newQtty >= 0) {
+      const newProducts = products;
+      newProducts[i].quantity = newQtty;
+      setProducts(newProducts);
+      setTotal(computeTotalCart(products));
+    }
+  };
+
+  useEffect(() => {
+    receiveProducts();
+  });
+
+  console.log(total);
   return (
     <section className="customer-products">
       <NavClient selected="produtos" customer="Nome usuário" showProducts />
       <div className="card_shelf">
-        <ProductCard
-          name="produto"
-          value="2.76"
-          image="https://m.media-amazon.com/images/I/61RUepIpfGS._AC_SL1000_.jpg"
-          quantity="6"
-        />
+        {products.length > 0 && (
+          products.map((p, i) => (
+            <ProductCard
+              key={ `${p.name + i}` }
+              name={ p.name }
+              value={ p.value }
+              image={ p.image }
+              quantity={ p.quantity }
+              id={ i }
+              quantityHandler={ changeQuantity }
+            />
+          ))
+        )}
       </div>
     </section>
   );
