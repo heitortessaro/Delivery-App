e2d3b59eda2e6201c35251dab8b1397d8b039e25
Gabriel Sartori <gsgabriel27@hotmail.com>
2022-09-26 18:06:43 -0300

    fix: bug on Register page

--- back-end/src/services/user.service.js
+++ back-end/src/services/user.service.js
@@ -35,7 +35,8 @@ class UserService {
     if (registeredUser) throw new CustomError(409, 'User already registered');
     await this.userModel
       .create({ name, email, password: hashedPassword, role: 'customer' });
-    return { message: 'Created' };
+    const newUser = await this.userModel.findOne({where: {name, email, password: hashedPassword}});
+    return { message: 'Created', newUser };
   }
 }
 
