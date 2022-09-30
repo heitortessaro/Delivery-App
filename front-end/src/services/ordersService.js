defeb1e45207064a6eca8c9ee2fed2f7374c595b
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-09-30 16:17:01 -0300

    feat: create loggin verification function

--- front-end/src/services/ordersService.js
+++ front-end/src/services/ordersService.js
@@ -3,14 +3,23 @@ const axios = require('axios');
 const URL = 'http://localhost:3001/sales';
 
 const getOrders = async (user) => {
-  // console.log(user);
   try {
     const result = await axios.get(URL, { headers: {
       authorization: user.token,
     } });
-    // console.log(result.data);
-    // // const orders = addQuantityKey(result.data);
     return result.data;
+  } catch (error) {
+    return [];
+  }
+};
+
+const verifyLogin = async (user) => {
+  try {
+    const result = await axios.get(URL, { headers: {
+      authorization: user.token,
+    } });
+    console.log(result);
+    return result.status;
   } catch (error) {
     return [];
   }
@@ -29,4 +38,4 @@ const returnDate = (date) => {
   return (`${day}/${month}/${year}`);
 };
 
-module.exports = { getOrders, returnDate };
+module.exports = { getOrders, returnDate, verifyLogin };
